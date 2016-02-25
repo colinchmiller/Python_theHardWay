3a49ae33be0ab86bf1b8082e7fbfc57b52532747
colinmiller <colinchmiller@gmail.com>
2016-02-24 22:20:47 -0600

    basic math exercise with example of temperature conversion

---
+++ basic conversions.py
@@ -0,0 +1,8 @@
+print "Temperature conversion from fahrenheit to celsius?: C=(F-32)*(5/9)"
+
+#need to have the decimals in place, otherwise the answers will not carry use decimals and will return 0
+print "100 degrees fahrenheit in celsius: ", (100-32)*(5.0/9.0)
+print "0 degrees fahrenheit in celsius: ", (0-32)*(5.0/9.0)
+
+print "test: ", 2+2
+print "test2: ", (2+2)
