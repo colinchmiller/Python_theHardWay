8c43fdc125c827975f7a3baa46d316d890398a24
Colin Miller <Colin Miller>
2016-03-19 21:33:34 -0500

    exercise 7

--- ex6.py
+++ ex6.py
@@ -1,6 +1,8 @@
+#first print, %d is the 10 following the text, also assigning a variable to be used later
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
+#order only seems to matter with the % assignment
 y = "Those who know %s and those who %s." % (binary, do_not)
 
 print (x)
@@ -17,4 +19,5 @@ print (joke_evaluation % hilarious)
 w = "This is the left side of..."
 e = "a string with a right side."
 
+#print the concatenation of two variables
 print (w + e)
