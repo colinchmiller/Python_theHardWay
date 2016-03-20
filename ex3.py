6d7a1aebb6754bb51068b77c5e08300450df6224
Colin Miller <Colin Miller>
2016-03-19 21:15:24 -0500

    parenthesized prints for previous lessons to allow function in python version 3

--- ex3.py
+++ ex3.py
@@ -1,32 +1,32 @@
 #printing the calculation context
-print "I will now count my chickens:"
+print ("I will now count my chickens:")
 
 #printing each group, the hens and the chickens
-print "Hens", 25+30/6
-print "Roosters", 100-25*3%4
+print ("Hens", 25+30/6)
+print ("Roosters", 100-25*3%4)
 
 #printing the next calculation context
-print "Now I will count the eggs:"
+print ("Now I will count the eggs:")
 
 #total number of eggs
-print 3+2+1-5+4%2-1/4+6
-print "floating point of the above: ", 3+2+1-5+4%2.0-1/4.0+6
+print (3+2+1-5+4%2-1/4+6)
+print ("floating point of the above: ", 3+2+1-5+4%2.0-1/4.0+6)
 
 #boolean question with math
-print "Is it true that 3+2<5-7?"
+print ("Is it true that 3+2<5-7?")
 
 #result from the above question
-print 3+2<5-7
+print (3+2<5-7)
 
 #breaking the question down to each side for comparison
-print "What is 3+2?", 3+2
-print "What is 5-7?", 5-7
+print ("What is 3+2?", 3+2)
+print ("What is 5-7?", 5-7)
 
-print "Oh, that's why it's False."
+print ("Oh, that's why it's False.")
 
-print "How about some more."
+print ("How about some more.")
 
 #more boolean examples
-print "Is it greater?", 5>-2
-print "Is it greater or equal?", 5>=-2
-print "Is it less or equal?", 5<=-2
+print ("Is it greater?", 5>-2)
+print ("Is it greater or equal?", 5>=-2)
+print ("Is it less or equal?", 5<=-2)
--- ex4.py
+++ ex4.py
@@ -8,12 +8,12 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 
-print "There are", cars, "cars available."
-print "There are only", drivers, "drivers available."
-print "There will be", cars_not_driven, "empty cars today."
-print "We can transport", carpool_capacity, "people today."
-print "We have", passengers, "to carpool today."
-print "We need to put about", average_passengers_per_car, "in each car."
+print ("There are", cars, "cars available.")
+print ("There are only", drivers, "drivers available.")
+print ("There will be", cars_not_driven, "empty cars today.")
+print ("We can transport", carpool_capacity, "people today.")
+print ("We have", passengers, "to carpool today.")
+print ("We need to put about", average_passengers_per_car, "in each car.")
 
 #if you get average_passengers_per_car = car_pool_capacity / passengers
 #NameError: name 'car_pool_capacity' is not defined,
